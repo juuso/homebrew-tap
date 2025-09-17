fc5c0122ca93272f4dbcc684879494075c3e6046
Juuso Salonen <juusojsalonen@gmail.com>
2025-09-17 09:17:54 +0300

    Update gigawatt to 0.1.0

--- Formula/gigawatt.rb
+++ Formula/gigawatt.rb
@@ -6,22 +6,22 @@ class Gigawatt < Formula
   on_macos do
     on_intel do
       url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-macos-x64.tar.gz'
-      sha256 '5029e0a2bbe85cd23a17f8e5fb604ea35ddb86640e7b9a09a501124477e7a10f'
+      sha256 '5310e9af54b2a2978bdb00d3c446c85e59a4ca0adf19dec1c58e13711cf51ddf'
     end
     on_arm do
       url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-macos-arm64.tar.gz'
-      sha256 '89b22a55a0997de61f21007c31baf402fd6c4a6411df28c4e97a082af9dc8b23'
+      sha256 '1541bcad0d74abb6cd418fa07efd621f715d49ac2a7845b8ad7d3794ffeee068'
     end
   end
 
   on_linux do
     on_intel do
       url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-linux-x64-musl.tar.gz'
-      sha256 'ddceb34d0b7cc3163c02dfd7d1e20b70587e155dc5faeb6e92542de8d7d7139d'
+      sha256 'db986ad89720620a2ef3c6a0a4d7452e12f237bad6a0f5cfd2f9e9fca287878c'
     end
     on_arm do
       url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-linux-arm64-musl.tar.gz'
-      sha256 '4aa386408a5af67c8de2415fe44ec20105d353cdd5a2a3f788672a8a8d55a6e2'
+      sha256 '2e1dbd37c7cfb483199c509f86460feadab2886ff6589369a42e4895693c4562'
     end
   end
 

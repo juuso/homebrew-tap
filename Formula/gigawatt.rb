c9fa4cb2bd6c024853e10a527df3bae3b4426566
Juuso Salonen <614446+juuso@users.noreply.github.com>
2025-09-19 13:11:00 +0300

    Update gigawatt to 0.1.1

--- Formula/gigawatt.rb
+++ Formula/gigawatt.rb
@@ -1,27 +1,27 @@
 class Gigawatt < Formula
-  desc 'An opinionated cross-theme, cross-platform, cross-shell prompt.'
-  homepage ''
-  version '0.1.0'
+  desc 'An opinionated theme-aware shell prompt'
+  homepage 'https://radiosilenceapp.com/gigawatt/'
+  version '0.1.1'
 
   on_macos do
     on_intel do
-      url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-macos-x64.tar.gz'
-      sha256 '5310e9af54b2a2978bdb00d3c446c85e59a4ca0adf19dec1c58e13711cf51ddf'
+      url 'https://github.com/juuso/gigawatt/releases/download/gigawatt-v0.1.1/gigawatt-macos-x64.tar.gz'
+      sha256 '32ef4b8a81b6a972eaf387f597af159c75cf87137c2e21ee24f6fbae1b6a5aca'
     end
     on_arm do
-      url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-macos-arm64.tar.gz'
-      sha256 '1541bcad0d74abb6cd418fa07efd621f715d49ac2a7845b8ad7d3794ffeee068'
+      url 'https://github.com/juuso/gigawatt/releases/download/gigawatt-v0.1.1/gigawatt-macos-arm64.tar.gz'
+      sha256 'a6128c7c840235e4f546af60cda2dc37e4b67828730e83103fa3103d222b90a3'
     end
   end
 
   on_linux do
     on_intel do
-      url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-linux-x64-musl.tar.gz'
-      sha256 'db986ad89720620a2ef3c6a0a4d7452e12f237bad6a0f5cfd2f9e9fca287878c'
+      url 'https://github.com/juuso/gigawatt/releases/download/gigawatt-v0.1.1/gigawatt-linux-x64-musl.tar.gz'
+      sha256 '45f336af7bd87f28d9c056fb2f8a34d5ec0361d864b5c17b25b43702b08166be'
     end
     on_arm do
-      url 'https://github.com/juuso/homebrew-tap/releases/download/gigawatt-v0.1.0/gigawatt-linux-arm64-musl.tar.gz'
-      sha256 '2e1dbd37c7cfb483199c509f86460feadab2886ff6589369a42e4895693c4562'
+      url 'https://github.com/juuso/gigawatt/releases/download/gigawatt-v0.1.1/gigawatt-linux-arm64-musl.tar.gz'
+      sha256 'e6c61002ec2713acafc194e833c7b3fe783552894301c5f839540a8d93456e04'
     end
   end
 
